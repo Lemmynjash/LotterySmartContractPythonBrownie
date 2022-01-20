bf242e2f8aeb561189a11912178e0f3f5ce7afba
Lemmynjash <lemmynjariakiarie@gmail.com>
2022-01-20 10:58:14 +0300

    now need to generate a pseudo random numbers

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 1. Users can enter lotter with ETH based on a USD fee
 2. An admin will choose when the lottery is over
 3. The lottery will select a random winner
+
+
+Ways to test this
+
+`mainet-fork`
+`development` with mocks
+`testnet` 
--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,14 +1,53 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
+import "@chainlink/contracts/src/v0.7/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Lottery {
+contract Lottery is Ownable {
     address payable[] public players; // will going to use this to track all the players
 
-    function enter() public payable {}
+    uint256 public usdEntryFee;
+    AggregatorV3Interface internal ethToUSDpriceFeed;
+    enum LOTTERY_STATE {
+        OPEN,
+        CLOSED,
+        CALCULATING_WINNER
+    }
 
-    function getEntranceFee() public {}
+    LOTTERY_STATE public lottery_state;
 
-    function startLottery() public {}
+    constructor(address _priceFeedAddress) {
+        usdEntryFee = 50 * (10**18); //50 USD multiplied by 10 raised to the 18th
+        ethToUSDpriceFeed = AggregatorV3Interface(_priceFeedAddress);
+        lottery_state = LOTTERY_STATE.CLOSED;
+    }
 
-    function endLottery() public {}
+    function enter() public payable {
+        require(lottery_state == LOTTERY_STATE.OPEN);
+        require(
+            msg.value >= getEntranceFee(),
+            "You dont have enough ETH! , insufficient balance :("
+        );
+        players.push(payable(msg.sender));
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        (, int256 price, , , ) = ethToUSDpriceFeed.latestRoundData();
+        uint256 adjustedPrice = uint256(price) * 10**10; //18 decimals
+        //$50, $2,000/ETH
+        //50/2,000
+        // 50* 100000 / 2000
+        uint256 costToEnter = (usdEntryFee * 10**18) / adjustedPrice;
+        return costToEnter;
+    }
+
+    function startLottery() public onlyOwner {
+        require(
+            lottery_state == LOTTERY_STATE.CLOSED,
+            "You cant start a new lottery yet!"
+        );
+        lottery_state = LOTTERY_STATE.OPEN;
+    }
+
+    function endLottery() public onlyOwner {}
 }
